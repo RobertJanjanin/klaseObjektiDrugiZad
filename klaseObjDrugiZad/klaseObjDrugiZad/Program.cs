5f4a3f5b964a74b2dd7305dd15212d80c7752f24
RobertJanjanin <55134583+RobertJanjanin@users.noreply.github.com>
2021-02-16 18:12:38 +0100

    Add files via upload

---
+++ klaseObjDrugiZad/klaseObjDrugiZad/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klaseObjDrugiZad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            {
+                Cetverokut a = new Cetverokut();
+
+                Trokut b = new Trokut();
+
+                a.UcitajStranice();
+
+                b.UcitajStranice();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
